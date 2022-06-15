eb4ee06d4f672d347f5440f5a114f535a1c396d9
douglasdl <douglas_san@hotmail.com>
2022-06-15 20:18:27 -0300

    Add Messages and Resources classes

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Play the game [here](https://douglasdl.github.io/Tower-Defense/)
 
 
 [Google Fonts](https://fonts.google.com/specimen/Orbitron)
+
+[Mobile Game Graphics](https://graphicriver.net/user/space_gecko/portfolio)
--- script.js
+++ script.js
@@ -23,8 +23,8 @@ const resources = [];
 
 // Mouse
 const mouse = {
-    x: undefined,
-    y: undefined,
+    x: 10,
+    y: 10,
     width: 0.1,
     height: 0.1,
 }
@@ -54,8 +54,8 @@ class Cell {
         this.height = cellSize;
     }
     draw() {
-        ctx.strokeStyle = 'black';
         if(mouse.x && mouse.y && collision(this, mouse)) {
+            ctx.strokeStyle = 'black';
             //ctx.lineWidth = 3;
             ctx.strokeRect(this.x, this.y, this.width, this.height);
         }
@@ -63,15 +63,15 @@ class Cell {
 }
 
 function createGrid() {
-    for(let y = cellSize; y <= canvas.height; y += cellSize) {
-        for(let x = 0; x <= canvas.width; x += cellSize) {
+    for(let y = cellSize; y < canvas.height; y += cellSize) {
+        for(let x = 0; x < canvas.width; x += cellSize) {
             gameGrid.push(new Cell(x, y));
         }
     }
 }
 createGrid();
 
-function hadleGameGrid() {
+function handleGameGrid() {
     for(let i = 0; i < gameGrid.length; i++) {
         gameGrid[i].draw();
     }
@@ -104,14 +104,14 @@ function handleProjectiles() {
         projectiles[i].draw();
         
         for(let j = 0; j < enemies.length; j++) {
-            if(enemies[j] && projectiles[i] && collision(enemies[j], projectiles[i])) {
+            if(enemies[j] && projectiles[i] && collision(projectiles[i], enemies[j])) {
                 enemies[j].health -= projectiles[i].power;
                 projectiles.splice(i, 1);
                 i--;
             }
         }
 
-        if(projectiles[i] && projectiles[i].x >= canvas.width - cellSize) {
+        if(projectiles[i] && projectiles[i].x > canvas.width - cellSize) {
             projectiles.splice(i, 1);
             i--;
         }
@@ -135,33 +135,19 @@ class Defender {
         ctx.fillRect(this.x, this.y, this.width, this.height);
         ctx.fillStyle = 'gold';
         ctx.font = '30px Orbitron';
-        ctx.fillText(Math.floor(this.health), this.x + 25, this.y + 30);
+        ctx.fillText(Math.floor(this.health), this.x + 15, this.y + 30);
     }
     update() {
         if(this.shooting) {
             this.timer++;
             if(this.timer % 100 === 0) {
-                projectiles.push(new Projectile(this.x + 70, this.y + this.height / 2));
+                projectiles.push(new Projectile(this.x + 70, this.y + 50));
             }
         } else {
             this.timer = 0;
         }
     }
 }
-
-canvas.addEventListener('click', function() {
-    const gridPositionX = mouse.x - (mouse.x % cellSize) + cellGap;
-    const gridPositionY = mouse.y - (mouse.y % cellSize) + cellGap;
-    if(gridPositionY < cellSize) return;
-    for(let i = 0; i < defenders.length; i++) {
-        if(defenders[i].x === gridPositionX && defenders[i].y === gridPositionY) return;
-    }
-    let defenderCost = 100;
-    if(numberOfResources >= defenderCost) {
-        defenders.push(new Defender(gridPositionX, gridPositionY));
-        numberOfResources -= defenderCost;
-    }
-})
 
 function handleDefenders() {
     for(let i = 0; i < defenders.length; i++) {
@@ -188,11 +174,49 @@ function handleDefenders() {
     }
 }
 
+// Floating Messages
+const floatingMessages = [];
+class FloatingMessage {
+    constructor(value, x, y, size, color) {
+        this.value = value;
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.lifeSpan = 0;
+        this.color = color;
+        this.opacity = 1;
+    }
+    update() {
+        this.y -= 0.3;
+        this.lifeSpan++;
+        if(this.opacity > 0.03) this.opacity -= 0.03;
+    }
+    draw() {
+        ctx.globalAlpha = this.opacity;
+        ctx.fillStyle = this.color;
+        ctx.font = this.size + 'px Orbitron';
+        ctx.fillText(this.value, this.x, this.y);
+        ctx.globalAlpha = 1;
+    }
+}
+
+function handleFloatingMessages() {
+    for(let i = 0; i < floatingMessages.length; i++) {
+        floatingMessages[i].update();
+        floatingMessages[i].draw();
+        if(floatingMessages[i].lifeSpan >= 50) {
+            floatingMessages.splice(i, 1);
+            i--;
+        }
+    }
+}
+
+
 // Enemies
 class Enemy {
     constructor(verticalPosition) {
         this.x = canvas.width;
-        this.y = verticalPosition + cellGap;
+        this.y = verticalPosition;
         this.width = cellSize - cellGap * 2;
         this.height = cellSize - cellGap * 2;
         this.speed = Math.random() * 0.2 + 0.4;
@@ -208,7 +232,7 @@ class Enemy {
         ctx.fillRect(this.x, this.y, this.width, this.height);
         ctx.fillStyle = 'black';
         ctx.font = '30px Orbitron';
-        ctx.fillText(Math.floor(this.health), this.x + 25, this.y + 30);
+        ctx.fillText(Math.floor(this.health), this.x + 15, this.y + 30);
     }
 }
 
@@ -224,6 +248,8 @@ function handleEnemies() {
 
         if(enemies[i].health <= 0) {
             let gainedResources = enemies[i].maxHealth / 10;
+            floatingMessages.push(new FloatingMessage('+ ' + gainedResources, enemies[i].x, enemies[i].y, 30, 'black'));
+            floatingMessages.push(new FloatingMessage('+ ' + gainedResources, 250, 40, 30, 'gold'));
             numberOfResources += gainedResources;
             score += gainedResources;
             const findThisIndex = enemyPositions.indexOf(enemies[i].y);
@@ -267,6 +293,8 @@ function handleResources() {
         resources[i].draw();
         if(resources[i] && mouse.x && mouse.y && collision(resources[i], mouse)) {
             numberOfResources += resources[i].amount;
+            floatingMessages.push(new FloatingMessage('+ ' + resources[i].amount, resources[i].x, resources[i].y, 30, 'black'));
+            floatingMessages.push(new FloatingMessage('+ ' + resources[i].amount, 300, 80, 30, 'gold'));
             resources.splice(i, 1);
             i--;
         }
@@ -295,16 +323,33 @@ function handleGameStatus() {
     }
 }
 
+canvas.addEventListener('click', function() {
+    const gridPositionX = mouse.x - (mouse.x % cellSize) + cellGap;
+    const gridPositionY = mouse.y - (mouse.y % cellSize) + cellGap;
+    if(gridPositionY < cellSize) return;
+    for(let i = 0; i < defenders.length; i++) {
+        if(defenders[i].x === gridPositionX && defenders[i].y === gridPositionY) return;
+    }
+    let defenderCost = 100;
+    if(numberOfResources >= defenderCost) {
+        defenders.push(new Defender(gridPositionX, gridPositionY));
+        numberOfResources -= defenderCost;
+    } else {
+        floatingMessages.push(new FloatingMessage('Need more resources', mouse.x, mouse.y, 20, 'black'));
+    }
+});
+
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = '#222';
     ctx.fillRect(0, 0, controlsBar.width, controlsBar.height);
-    hadleGameGrid();
+    handleGameGrid();
     handleResources();
     handleDefenders();
     handleProjectiles();
     handleEnemies();
     handleGameStatus();
+    handleFloatingMessages();
     frame++;
     if(!gameOver) requestAnimationFrame(animate);
     
